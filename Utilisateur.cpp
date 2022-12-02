feea587988545aec611667251522acca759c383e
AzoulayNathan <107265738+AzoulayNathan@users.noreply.github.com>
2022-12-02 15:39:06 +0100

    Update Utilisateur.cpp

--- Utilisateur.cpp
+++ Utilisateur.cpp
@@ -1,1 +1,31 @@
 #include "Utilisateur.h"
+
+Utilisateur::Utilisateur(char nom, char prenom, int age, char position)
+{
+    this->nom = nom;
+    this->prenom = prenom;
+  this->position = position;
+  this->age = age;
+}
+
+void Utilisateur::pieds();
+{
+    this->canWalk() = true;
+}
+
+void Utilisateur::metro();
+{
+    this->GetSub() = true;
+}
+
+void Utilisateur::car();
+{
+    this-> getCar = true;
+
+}
+
+void Utilisateur:: velo();
+{
+    this->GetBike = true;
+
+}
