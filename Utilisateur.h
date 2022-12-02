f55823335192e04982c57eb0b048934d87b1c438
AzoulayNathan <107265738+AzoulayNathan@users.noreply.github.com>
2022-12-02 15:28:50 +0100

    Update Utilisateur.h

--- Utilisateur.h
+++ Utilisateur.h
@@ -1,7 +1,25 @@
 class Utilisateur {
+    private :
+char nom ;
+char prenom;
+int age;
+char position; // Position de l'utilisateur
+bool voiture;
+bool velo;
+bool metro;
+bool pieds;
 
-    
 public:
+// Constructeurs
+Utilisateur();
+Utilisateur (char nom, char prenom,int age, bool voiture, bool velo, bool metro, bool pieds);
 
-    
+//Accesseurs et mutateurs
+void setPosition(char position);
+void canWalk() ;
+void GetBike();
+void GetCar();
+void GetSub();
 } ;
+
+   
